b22cd9d994e72ad9ac598616de3aa5fd8448fc82
lukeemail <kjd1760@gmail.com>
2020-05-10 01:02:57 +0800

    debugger at create close #3

--- app/controllers/codes_controller.rb
+++ app/controllers/codes_controller.rb
@@ -23,9 +23,13 @@ class CodesController < ApplicationController
 
   # POST /codes
   # POST /codes.json
-  def create
-    @code = Code.new(code_params)
-    debugger
+    def create
+      # debugger
+      @code = Code.new(
+        code_params.merge(
+        result: docker run ruby ruby -e '#{params[:code][:user]}'.strip
+        )
+      )
 
     respond_to do |format|
       if @code.save
